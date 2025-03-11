8bf100a2b96b72c8c4aa389fcd482245134cda70
SahilSimmaUCI <ssimma@uci.edu>
2025-03-11 20:24:00 +0000

    Function declarations

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,4 +1,4 @@
-#include 'dijkstras.h'
+#include "dijkstras.h"
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous);
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,6 +1,6 @@
-#include 'ladder.h'
+#include "ladder.h"
 
-void error(string word1, string word2, string msg);
+void error(string word1, string word2, string msg) {std::cout << "Key Error 1: " << word1 << "Key Error 2: " << word2 << "\nError message: " << msg << std::endl;}
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
 bool is_adjacent(const string& word1, const string& word2);
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
